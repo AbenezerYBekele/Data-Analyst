f4311fb36d66055b5b21fba9b502b93c134475ac
Abenezer Y Bekele <113382918+AbenezerYBekele@users.noreply.github.com>
2025-09-06 15:57:36 -0500

    Update covid-19 vaccine.sql

--- SQL/covid-19 vaccine.sql
+++ SQL/covid-19 vaccine.sql
@@ -7,7 +7,6 @@ FROM [vaccination-data]
 ORDER BY [TOTAL VACCINATIONS] DESC
 LIMIT 1;
 
--- Top 5 Countries with the Highest Total Vaccinations
 SELECT 
     COUNTRY,
     [TOTAL VACCINATIONS] 
@@ -15,7 +14,6 @@ FROM "vaccination-data"
 ORDER BY [TOTAL VACCINATIONS]  DESC
 LIMIT 5;
 
--- Countries with the Highest Percentage of Population Vaccinated (1+ Dose)
 SELECT 
     COUNTRY,
     PERSONS_VACCINATED_1PLUS_DOSE_PER100 AS "Vaccinated 1+ Dose per 100"
@@ -24,7 +22,6 @@ WHERE PERSONS_VACCINATED_1PLUS_DOSE_PER100 IS NOT NULL
 ORDER BY PERSONS_VACCINATED_1PLUS_DOSE_PER100 DESC
 LIMIT 5;
 
--- Countries with Most Booster Doses Administered
 SELECT 
     COUNTRY,
     PERSONS_BOOSTER_ADD_DOSE
@@ -32,7 +29,6 @@ FROM "vaccination-data"
 ORDER BY PERSONS_BOOSTER_ADD_DOSE DESC
 LIMIT 5;
 
--- Vaccination Rates Across Regions
 SELECT 
     WHO_REGION,
     SUM("TOTAL VACCINATIONS") AS total_vaccinations,
@@ -41,7 +37,6 @@ FROM "vaccination-data"
 GROUP BY WHO_REGION
 ORDER BY total_vaccinations DESC;
 
--- Countries with Most Booster Doses Administered
 SELECT
 	COUNTRY,
 	PERSONS_BOOSTER_ADD_DOSE
@@ -49,13 +44,11 @@ FROM "vaccination-data"
 ORDER BY PERSONS_BOOSTER_ADD_DOSE DESC
 LIMIT 5;
 
--- Countries That Have Not Administered Any Booster Doses
 SELECT 
     COUNTRY
 FROM "vaccination-data"
 WHERE PERSONS_BOOSTER_ADD_DOSE = 0;
 
--- Total Vaccinations Per Region with Comparison to Population
 SELECT 
     WHO_REGION,
     SUM("TOTAL VACCINATIONS") AS [Total vaccinations],
@@ -64,7 +57,6 @@ FROM "vaccination-data"
 GROUP BY WHO_REGION
 ORDER BY [Total vaccinations] DESC;
 
--- Countries with the Most Recent Vaccine Usage
 SELECT 
     COUNTRY,
     DATE_UPDATED AS [Date updste]
@@ -72,14 +64,12 @@ FROM "vaccination-data"
 ORDER BY DATE_UPDATED DESC
 LIMIT 5;
 
--- Title: Total Vaccinations by Vaccine Type
 SELECT 
     SUM("TOTAL VACCINATIONS") AS [Total vaccinations]
 FROM "vaccination-data"
 GROUP BY VACCINES_USED
 ORDER BY [Total vaccinations] DESC;
 
--- Percentage of Population Vaccinated with Last Dose
 SELECT 
     COUNTRY,
     PERSONS_LAST_DOSE_PER100 AS [PERSONS LAST DOSE PER100]
@@ -87,7 +77,6 @@ FROM "vaccination-data"
 ORDER BY PERSONS_LAST_DOSE_PER100 DESC
 LIMIT 5;
 
--- Total Vaccinations by Date
 SELECT 
     DATE_UPDATED AS [Date update],
     SUM("TOTAL VACCINATIONS") AS [Total Vaccinations]
@@ -95,7 +84,6 @@ FROM "vaccination-data"
 GROUP BY DATE_UPDATED
 ORDER BY DATE_UPDATED DESC;
 
--- Vaccination Data by WHO Region with Booster Dose Analysis
 SELECT 
     WHO_REGION,
     COUNT(DISTINCT COUNTRY) AS [Number of countries],
@@ -106,6 +94,7 @@ SELECT
 FROM "vaccination-data"
 GROUP BY WHO_REGION
 ORDER BY [Total vaccinations in region] DESC;
+
 
 
 
