ca639b2dec65edeb09b878b3b99f210295f903f9
Abenezer Y Bekele <113382918+AbenezerYBekele@users.noreply.github.com>
2025-09-06 15:58:23 -0500

    Update Obesitysql.sql

--- SQL/Obesitysql.sql
+++ SQL/Obesitysql.sql
@@ -1,4 +1,4 @@
--- Calculate average obesity rate, highest and lowest obesity rates by state
+
 SELECT 
     COUNT(*) AS [Total states],
 	ROUND(AVG(Obesity), 2) AS [Average Obesity Rate],
@@ -6,14 +6,12 @@ SELECT
     MIN(Obesity) AS [lowest obesity rate]
 FROM [national obesity by state];
 
--- Rank states by obesity rate in descending order
 SELECT 
     NAME AS state, 
     Obesity AS [obesity rate],
     RANK() OVER (ORDER BY Obesity DESC) AS rank
 FROM [national obesity by state];
 
--- Get the top 5 states with the highest obesity rates
 SELECT 
     NAME AS state, 
     Obesity AS [Obesity rate]
@@ -21,7 +19,6 @@ FROM [national obesity by state]
 ORDER BY Obesity DESC
 LIMIT 5;
 
--- Get the 5 states with the lowest obesity rates
 SELECT 
     NAME AS state, 
     Obesity AS [Obesity rate]
@@ -29,20 +26,17 @@ FROM [national obesity by state]
 ORDER BY Obesity ASC
 LIMIT 5;
 
--- median Obesity
 SELECT Obesity
 FROM [national obesity by state]
 ORDER BY Obesity
 LIMIT 1 OFFSET (SELECT COUNT(*)/2 FROM [national obesity by state]);
 
--- Calculate the Average Obesity Rate Across All States
 SELECT 
     NAME AS state, 
     Obesity AS "Obesity Percentage"
 FROM [national obesity by state]
 ORDER BY Obesity DESC;
 
--- Categorize States Based on Obesity Rate
 SELECT NAME AS State,
        Obesity,
        CASE 
@@ -52,12 +46,10 @@ SELECT NAME AS State,
        END AS 'Obesity Category'
 FROM [national obesity by state];
 
---Count the Number of States with Obesity Rates Above 30%
 SELECT COUNT(*) AS 'States Above 30'
 FROM [national obesity by state]
 WHERE Obesity > 30;
 
---Find the State with the Largest Area and Its Obesity Rate
 SELECT NAME AS State, 
        ROUND(CAST(Shape__Area AS REAL) , 2) AS 'Largest Area',
        Obesity AS Obesity_Percentage
@@ -65,7 +57,6 @@ FROM [national obesity by state]
 ORDER BY Shape__Area DESC
 LIMIT 1;
 
---Query to Categorize States by Obesity Level
 SELECT NAME AS State,
        Obesity AS 'Obesity Percentage',
        CASE 
@@ -74,9 +65,6 @@ SELECT NAME AS State,
          ELSE 'Low'
        END AS 'Obesity Level'
 FROM [national obesity by state];
-
-
---Query to Count States by Obesity Category
 
 SELECT 
     CASE 
@@ -87,6 +75,7 @@ SELECT
     COUNT(*) AS State_Count
 FROM [national obesity by state]
 GROUP BY Obesity_Level;
+
 
 
 
